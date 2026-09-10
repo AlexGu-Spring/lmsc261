e7d623873c831b5ea96c3a21a791d4c5f6648a54
AlexGu-Spring <agu2@berklee.edu>
2026-09-10 14:11:58 -0400

    Update hello_input.js

--- hello_input.js
+++ hello_input.js
@@ -1,6 +1,2 @@
 let nameToPrint = prompt("What's your name?");
-<<<<<<< HEAD
-print("Hi" + nameToPrint)
-=======
-print("Hi" + nameToPrint)
->>>>>>> 8162dd7f22d1b0a3f07d986b2c3c191a08e22221
+print("Hello, " + nameToPrint);
