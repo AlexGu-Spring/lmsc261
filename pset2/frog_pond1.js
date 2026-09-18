664e4e006006efafba3de83e8405b82e6432730b
AlexGu-Spring <agu2@berklee.edu>
2026-09-18 19:52:57 -0400

    html

--- pset2/frog_pond1.js
+++ pset2/frog_pond1.js
@@ -5,6 +5,6 @@ if(isPondOverCapacity){
     let messageToPrint = "Come on in!";
     print(messageToPrint);
 } else {
-    let messageToPrint = "It’s too crowded!";
+    let messageToPrint = "It's too crowded!";
     print(messageToPrint);
 }
