a9b876293d09768dcc3086e74c54f5b4560af92f
AlexGu-Spring <agu2@berklee.edu>
2026-09-18 19:50:16 -0400

    pond1

--- pset2/frog_pond1.js
+++ pset2/frog_pond1.js
@@ -1,1 +1,10 @@
-print("1")
+const  maxFrogCapacity = 15;
+let numFrogs = prompt("How many frogs are about to jump in?");
+isPondOverCapacity = numFrogs <= maxFrogCapacity;
+if(isPondOverCapacity){
+    let messageToPrint = "Come on in!";
+    print(messageToPrint);
+} else {
+    let messageToPrint = "It’s too crowded!";
+    print(messageToPrint);
+}
